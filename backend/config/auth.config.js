a87debc0370e86dcc335a968c4a147bbeb00e4f5
Jesse Songok <jessesongok@gmail.com>
2022-02-28 12:16:55 +0300

    Backend User Auth and Role Management

---
+++ backend/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "myAccounts-secret-key"
+};
---
+++ backend/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: process.env.PG_HOST,
+    USER: process.env.PG_USER,
+    PASSWORD: process.env.PG_PASSWORD,
+    DB: process.env.PG_DB,
+    dialect: "postgres",
+};
